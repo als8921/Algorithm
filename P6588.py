1ee9c8b7672332e78be52ba47aab64e8324eed12
als8921 <als8921@naver.com>
2022-10-08 20:53:03 +0900

    1008

---
+++ P6588.py
@@ -0,0 +1,21 @@
+
+table = [False, False] + [True]*(1000000)
+idx = 2
+while(idx < 1000):
+    if(table[idx]):
+        for i in range(2 * idx, 1000001, idx):
+            table[i] = False
+    idx+=1
+
+while(1):
+    num = int(input())
+    if(num == 0):
+        break
+    isTrue = False
+    for i in range(3, num):
+        if(table[i] and table[num-i]):
+            print(f"{num} = {i} + {num-i}")
+            isTrue = True
+            break
+    if(not isTrue):
+        print("Goldbach's conjecture is wrong.")
