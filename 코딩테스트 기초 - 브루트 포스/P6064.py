c95eb50208dae0a7845e9e155fd0bba91c6604ed
als8921 <als8921@naver.com>
2023-01-18 00:19:16 +0900

    P6064

--- 코딩테스트 기초 - 브루트 포스/P6064.py
+++ 코딩테스트 기초 - 브루트 포스/P6064.py
@@ -1,15 +1,11 @@
 n = int(input())
 for _ in range(n):
     M, N, x, y = map(int, input().split())
-    X, Y = x, y
-    while(True):
-        if(X > M*N or Y > M*N):
+    while(1):
+        if(x > M*N):
             print(-1)
             break
-        if(X == Y):
-            print(X)
+        if((x-y) % N == 0):
+            print(x)
             break
-        elif(X < Y):
-            X += M
-        elif(X > Y):
-            Y += N
+        x += M
