dd7194654fd551d2858ecd409329fddbc0ed6f6f
als8921 <als8921@naver.com>
2024-09-27 15:16:52 +0900

    파일 정리

--- Study/ClassPrac/main.py
+++
@@ -1,6 +0,0 @@
-from module.user import User
-
-UserA = User("AAAAA", 23)
-UserA.getInfo()
-UserA.updateAge(24)
-UserA.getInfo()
--- etc/P10828.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-cmd = []
-stack = []
-for i in range(n):
-    cmd.append(input())
-    if(cmd[:4]=="push"):
-        print(cmd[:4], cmd[5:])
-        stack.append(cmd[5:])
