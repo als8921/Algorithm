bd3928fbb99094773af2f0e40a8e9f30c76c9fda
als8921 <als8921@naver.com>
2024-09-29 08:58:08 +0900

    P2667 단지번호붙이기

--- 알고리즘 기초 - 그래프1/P2667.단지번호붙이기.py
+++ 알고리즘 기초 - 그래프1/P2667.단지번호붙이기.py
@@ -1,42 +1,40 @@
 count = 0
-def solution():
-    n = int(input())
+n = int(input())
 
-    graph = []
-    visited = [[0] * n for _ in range(n)]
+graph = []
+visited = [[0] * n for _ in range(n)]
 
-    for i in range(n):
-        graph.append(input())
+for i in range(n):
+    graph.append(input())
 
-    dx = (-1, 1, 0, 0)
-    dy = (0, 0, -1, 1)
+dx = (-1, 1, 0, 0)
+dy = (0, 0, -1, 1)
 
-    stack = []
-    result = []
+stack = []
+result = []
+def bfs(x, y):
+    visited[y][x] = 1
+    global count
+    count += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-    def bfs(x, y):
-        count += 1
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if(nx >= 0 and nx < n and ny >= 0 and ny < n):
-                if(graph[ny][nx] == "1" and visited[ny][nx] == 0):
-                    visited[ny][nx] = 1
-                    bfs(nx, ny)
-
-
-
-    for x in range(n):
-        for y in range(n):
-            if(graph[y][x] == "1"):
-               bfs(x, y)
-    
-    print(count)
-    # result.sort()
-    # print(len(result))
-    # for i in result:
-    #     print(i)
+        if(nx >= 0 and nx < n and ny >= 0 and ny < n):
+            if(graph[ny][nx] == "1" and visited[ny][nx] == 0):
+                visited[ny][nx] = 1
+                bfs(nx, ny)
 
 
-solution()
+
+for x in range(n):
+    for y in range(n):
+        if(graph[y][x] == "1" and visited[y][x] == 0):
+            bfs(x, y)
+            result.append(count)
+            count = 0
+
+print(len(result))
+result.sort()
+for i in result:
+    print(i)
