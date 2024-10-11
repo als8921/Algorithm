7a9aa0af7aaa4a654a7e9ee5e3cee60efa0ab31f
als8921 <als8921@naver.com>
2024-10-11 16:27:00 +0900

    P16929.Two Dots

---
+++ 알고리즘 기초 - 그래프1 (연습)/P16929.Two Dots.py
@@ -0,0 +1,74 @@
+"""
+문제
+Two Dots는 Playdots, Inc.에서 만든 게임이다. 게임의 기초 단계는 크기가 N x M인 게임판 위에서 진행된다.
+
+
+
+각각의 칸은 색이 칠해진 공이 하나씩 있다. 이 게임의 핵심은 같은 색으로 이루어진 사이클을 찾는 것이다.
+
+다음은 위의 게임판에서 만들 수 있는 사이클의 예시이다.
+
+	
+점 k개 d1, d2, ..., dk로 이루어진 사이클의 정의는 아래와 같다.
+
+모든 k개의 점은 서로 다르다. 
+k는 4보다 크거나 같다.
+모든 점의 색은 같다.
+모든 1 ≤ i ≤ k-1에 대해서, di와 di+1은 인접하다. 또, dk와 d1도 인접해야 한다. 두 점이 인접하다는 것은 각각의 점이 들어있는 칸이 변을 공유한다는 의미이다.
+게임판의 상태가 주어졌을 때, 사이클이 존재하는지 아닌지 구해보자.
+
+입력
+첫째 줄에 게임판의 크기 N, M이 주어진다. 둘째 줄부터 N개의 줄에 게임판의 상태가 주어진다. 게임판은 모두 점으로 가득차 있고, 게임판의 상태는 점의 색을 의미한다. 점의 색은 알파벳 대문자 한 글자이다.
+
+출력
+사이클이 존재하는 경우에는 "Yes", 없는 경우에는 "No"를 출력한다.
+"""
+h, w = map(int, input().split())
+graph = []
+for _ in range(h):
+    graph.append(input())
+
+visited = [[0] * w for _ in range(h)]
+
+# 4 방향 설정 차례대로 순환
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+result = False
+
+def dfs(y, x, dir):
+    global result
+    # 같은 방향
+    ny, nx = y + dy[dir], x + dx[dir]
+    if(0 <= ny < h and 0 <= nx < w):
+        if(graph[y][x] == graph[ny][nx]):
+            visited[ny][nx] = 1
+            dfs(ny, nx, dir)
+
+    # 다른 방향
+    if(dir < 3):
+        ny, nx = y + dy[dir + 1], x + dx[dir + 1]
+        if(0 <= ny < h and 0 <= nx < w):
+            if(graph[y][x] == graph[ny][nx]):
+                if(dir == 2):
+                    result = True
+                visited[ny][nx] = 1
+                dfs(ny, nx, dir + 1)
+
+
+
+
+for y in range(h):
+    if(result):
+        break
+    for x in range(w):
+        if(result):
+            break
+        visited[y][x] = 1
+        dfs(y, x, 0)
+        visited = [[0] * w for _ in range(h)]
+
+
+if(result):
+    print("Yes")
+else:
+    print("No")
