40587ced007a7f04d34c0b10cb9b8a0f5c58b0fb
als8921 <als8921@naver.com>
2025-03-01 17:29:19 +0900

    P14442_벽_부수고_이동하기_2.cpp

---
+++ Cpp-Algorithm/0x09강 - BFS/P14442_벽_부수고_이동하기_2.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dy[] = {-1, 1, 0, 0};
+int dx[] = {0, 0, -1, 1};
+
+int N, M, K;
+
+char graph[1001][1001];
+int visited[1001][1001][11];
+int main()
+{
+    fill(&visited[0][0][0], &visited[0][0][0] + 1001 * 1001 * 11, 0);
+    cin >> N >> M >> K;
+
+    for(int y = 0; y < N; y++)
+        for(int x = 0; x < M; x++)
+            cin >> graph[y][x];
+
+    queue<vector<int>> Q;
+    Q.push({0, 0, 0});
+    visited[0][0][0] = 1;
+
+
+    while(!Q.empty())
+    {
+        int y = Q.front()[0];
+        int x = Q.front()[1];
+        int breakCount = Q.front()[2];
+        Q.pop();
+
+        if(y == N - 1 && x == M - 1)
+        {
+            cout << visited[y][x][breakCount] << "\n";
+            return 0;
+        }
+
+        for(int i = 0; i < 4; i++)
+        {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+            if(!visited[ny][nx][breakCount] && graph[ny][nx] == '0')
+            {
+                visited[ny][nx][breakCount] = visited[y][x][breakCount] + 1;
+                Q.push({ny, nx, breakCount});
+            }
+            if(breakCount + 1 <= K && !visited[ny][nx][breakCount + 1] && graph[ny][nx] == '1')
+            {
+                visited[ny][nx][breakCount + 1] = visited[y][x][breakCount] + 1;
+                Q.push({ny, nx, breakCount + 1});
+            }
+        }
+    }
+    cout << "-1\n";
+}
